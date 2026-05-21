88210ed645c8e73eeaa5e7ffc82bf0c4b8fb3352
pawankushwah23713-oss <pawankushwah23713@gmail.com>
2026-05-21 20:43:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 // 🔥 ONLY CHANGE HERE
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0" , () => {
   console.log(`Server Running On Port ${PORT}`);
 });
