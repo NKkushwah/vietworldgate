5d54505323ca79b151238ff9d2004dcb5afa8323
pawan kushwah <pawankushwah23713@gmail.com>
2026-05-25 13:37:49 +0530

    App. file change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
 import AppRoutes from "./routes/AppRoutes";
+import { 
+  FaRegEnvelopeOpen, 
+  FaArrowRight, 
+  FaFacebookF, 
+  FaInstagram, 
+  FaYoutube, 
+  FaLinkedinIn, 
+  FaWhatsapp, 
+  FaChevronUp,
+  FaRegEnvelope
+} from 'react-icons/fa';
+import "./App.css";
 
 function App() {
-  return <AppRoutes />;
+   const scrollToTop = (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return <>
+           <AppRoutes />
+    
+            <a href="#top" className="scroll-top" onClick={scrollToTop} aria-label="Scroll to top">
+              <FaChevronUp />
+            </a>
+        
+  </>
 }
 
 export default App;
