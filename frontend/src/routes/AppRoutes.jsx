c4e1a72e86f6cffe6ff90c43e0aafd5591ca84c9
NKkushwah <nandkishorek717@gmail.com>
2026-05-25 18:16:47 +0530

    ..

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,19 +5,20 @@ import ContactUs from "../pages/ContactUs";
 import CompanyProfile from "../pages/CompanyProfile";
 import MissionVision from "../pages/MissionVision";
 import ValuesGrid from "../pages/ValuesGrid";
-import OurTeam from "../pages/OurTeam";
+
 
 function AppRoutes() {
   return (
     <BrowserRouter>
 
       <Routes>
-        <Route path = "ourteam" element = {<OurTeam />} />
+       
         <Route path="/" element={<Dashboard />} />
         <Route path="/MissionVision" element={<MissionVision />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/companyprofile" element={<CompanyProfile/>} />
         <Route path="/ValuesGrid" element={<ValuesGrid/>} />
+
 
       </Routes>
 
